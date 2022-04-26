512a2e9641f39558d7c89627fea2fb85432e7b68
Meenakshi Sundaram N <100022215+msundaram03@users.noreply.github.com>
2022-04-26 22:07:37 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if (5<=a<=2500 && 5<=a<=2500){
+    printf("%d\n",a/b);
+    printf("%d",a%b);}
+  else{
+    return 0;}
+}
